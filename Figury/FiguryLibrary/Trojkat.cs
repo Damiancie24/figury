76e7a40db62d9d90e6a20becced52ceca117456b
Damiancie24 <d.cieszynski@microsoft.wsei.edu.pl>
2020-03-02 18:33:24 +0100

    Aplikacja trojkat

---
+++ Figury/FiguryLibrary/Trojkat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FiguryLibrary
+{
+    public class Trojkat
+    {
+        // Dane
+        double a; //this.a;
+        double b; //this.b;
+        double c; //this.c;
+
+        // konstuktory
+        public Trojkat()  //Domyslny konstruktor
+        {
+            a = b = c = 1.0;
+
+        }
+
+        public Trojkat(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+
+        }
+
+    }
+}
